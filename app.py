4bfb12e612c516e46852ea18d55dfb645f72ff19
Raman1717 <ramanagrawal4321@gmail.com>
2025-11-22 18:58:05 +0530

    port updated

--- app.py
+++ app.py
@@ -502,5 +502,6 @@ def delete_session(session_id):
 # RUN SERVER
 # =============================
 if __name__ == "__main__":
-    print("Server starting at: http://localhost:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT automatically
+    print(f"Server starting at: http://0.0.0.0:{port}")
+    app.run(debug=False, host="0.0.0.0", port=port)
